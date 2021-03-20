7c2be56d1cd8e3e8987d2f6c0172d7bc9ec92c5d
Vinayak Goswami <vgoswami714841@yahoo.com>
2021-03-20 13:09:35 +0530

    Update 20/03/2021

---
+++ 2189.cpp
@@ -0,0 +1,25 @@
+// https://cses.fi/problemset/task/2189
+
+#include <bits/stdc++.h>
+
+#define ll long double
+
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll x1, y1, x2, y2;
+
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        ll x3, y3;
+
+        cin >> x3 >> y3;
+
+    }
+}
